de93c17e3f66e498d0f3519d11c9855fef60f815
drhcastro <drhcastrovillela@gmail.com>
2025-08-19 12:30:00 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
+const CACHE_NAME = 'alimentacion-ac-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'app.js',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
